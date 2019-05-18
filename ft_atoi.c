a12cbb269ac70235fe8a1146dd40a50596b331f6
JonathanLimbada <50628608+JonathanLimbada@users.noreply.github.com>
2019-05-18 11:17:08 +0200

    Add files via upload
    added atoi and putendl

---
+++ ft_atoi.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: event <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/18 10:04:44 by event             #+#    #+#             */
+/*   Updated: 2019/05/18 11:00:38 by event            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	atoi(const char *str);
+{
+	int	negative;
+	int	result;
+
+	negative = 1;
+	result = 0;
+	while (*str && (*str == ' ' || *str == '\n' || *str == '\t' ||
+			*str == '\v' || *str == '\f' || *str == '\r'))
+		++str;
+	if (*str == '-')
+		negative = -1;
+	if (*str == '-' || *str == '+')
+		++str;
+	while (*str && *str >= '0' && *str <= '9')
+	{
+		res = res * 10 + (*str - 48);
+		++str;
+	}
+	return (res * negative);
+}
