1b81e881a095b9127d90d531648bf2388df23170
JonathanLimbada <50628608+JonathanLimbada@users.noreply.github.com>
2019-05-20 16:59:17 +0200

    Add files via upload
    libft.h added

---
+++ libft.h
@@ -0,0 +1,39 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <string.h>
+# include <stdlib.h>
+
+typedef strcut		s_list
+{
+	void	*content;
+	size_t	content_size;
+	struct	s_list	*next;
+}		t_list;
+
+int	ft_atoi(const char *str);
+void	ft_bzero(void *s, size_t n);
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putchar(char c);
+void	ft_putendl_fd(char const *s, int fd);
+void	ft_putendl(char const *s);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putnbr(int n);
+void	ft_putstr_fd(char const *s, int fd);
+void	ft_putstr(char const *s);
+void	ft_strclr(char *s);
+int	ft_strcmp(const char *s1, const char *s2);
+char	ft_strcpy(char *dst, const char *src);
+char	*ft_strdup(const char *s1);
+int	ft_strequ(char const *s1, char const *s2);
+int	ft_strlen(char *str);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strncpy(char *dst, const char *src, size_t len);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+#endif
